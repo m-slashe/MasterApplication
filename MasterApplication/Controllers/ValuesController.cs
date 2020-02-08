2b71f0ce8cece485ae4b62fd0ac6a3136dc45b6f
Murilo Vicente da Silva <murilo.silva@programmers.com.br>
2020-02-08 15:16:03 -0300

    Initial commit

---
+++ MasterApplication/Controllers/ValuesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryApi.Models.Values;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace MasterApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public IConfiguration _configuration { get; set; }
+        public ValuesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("listaProduto")]
+        public ActionResult<ProdutoModel> ListaProduto()
+        {
+            ProdutoModel produto = new ProdutoModel();
+            produto.Id = 6;
+            produto.Nome = "Teste";
+            produto.Sobrenome = "Seila";
+            produto.Environment = _configuration["Teste"];
+            return produto;
+        }
+    }
+}
